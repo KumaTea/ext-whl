18528e0aae89d57e873860aff0c4f7c44ded8032
KumaTea <KumaTea@outlook.com>
2021-03-21 05:01:50 +0800

    fix github page

--- docker/build-wheels.sh
+++ docker/build-wheels.sh
@@ -3,6 +3,6 @@
 set -ex
 
 # pip config set global.index-url https://mirrors.matrix.moe/pypi/web/simple
-pip install -U -v -r /root/packages.txt -f https://ext.maku.ml/wheels.html -f https://torch.maku.ml/whl/stable.html
+pip install -U -r /root/packages.txt -f https://ext.maku.ml/wheels.html -f https://torch.maku.ml/whl/stable.html
 mkdir /root/whl
 cp -a $(find /root/.cache/pip | grep whl | tr '\n' ' ') /root/whl/ || echo "No new whl is built!"
