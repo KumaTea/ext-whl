b2f8086b84731010e2109452c76cce6b1f4e6106
KumaTea <KumaTea@outlook.com>
2021-03-30 13:18:12 +0800

    update scripts

---
+++ build.bat
@@ -0,0 +1,23 @@
+cd docker
+REM docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py39 -f py39/Dockerfile .
+REM docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py38 -f py38/Dockerfile .
+REM docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py37 -f py37/Dockerfile .
+REM docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py36 -f py36/Dockerfile .
+
+REM docker image prune
+
+docker run -it --name py39e kumatea/ext:py39 bash /root/build-wheels.sh
+docker cp py39e:/root/whl .
+docker rm py39e
+
+docker run -it --name py38e kumatea/ext:py38 bash /root/build-wheels.sh
+docker cp py38e:/root/whl .
+docker rm py38e
+
+docker run -it --name py37e kumatea/ext:py37 bash /root/build-wheels.sh
+docker cp py37e:/root/whl .
+docker rm py37e
+
+docker run -it --name py36e kumatea/ext:py36 bash /root/build-wheels.sh
+docker cp py36e:/root/whl .
+docker rm py36e
--- docker/build-wheels.sh
+++ docker/build-wheels.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-# pip config set global.index-url https://mirrors.matrix.moe/pypi/web/simple
-pip install -U -r /root/packages-stdln.txt -f https://ext.maku.ml/wheels.html -f https://torch.maku.ml/whl/stable.html
-pip install -U -r /root/packages-deps.txt -f https://ext.maku.ml/wheels.html -f https://torch.maku.ml/whl/stable.html
+# pip config set global.index-url https://mirrors.vmatrix.org.cn/pypi/web/simple
+pip install -U -v -r /root/packages-stdln.txt -f https://ext.maku.ml/wheels.html -f https://torch.maku.ml/whl/stable.html
+pip install -U -v -r /root/packages-deps.txt -f https://ext.maku.ml/wheels.html -f https://torch.maku.ml/whl/stable.html
 mkdir /root/whl
 cp -a $(find /root/.cache/pip | grep whl | tr '\n' ' ') /root/whl/ || echo "No new whl is built!"
