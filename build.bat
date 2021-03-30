00284f449840c3de7fa11e999a3bbfa50dfcd1b5
KumaTea <KumaTea@outlook.com>
2021-03-30 23:09:32 +0800

    numpy 1.20.2 for armv7l

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 cd docker
-REM docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py39 -f py39/Dockerfile .
-REM docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py38 -f py38/Dockerfile .
-REM docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py37 -f py37/Dockerfile .
-REM docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py36 -f py36/Dockerfile .
+docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py39 -f py39/Dockerfile .
+docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py38 -f py38/Dockerfile .
+docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py37 -f py37/Dockerfile .
+docker buildx build --pull --load --platform linux/arm/v7 -t kumatea/ext:py36 -f py36/Dockerfile .
 
 REM docker image prune
 
--- docker/build-wheels.sh
+++ docker/build-wheels.sh
@@ -3,7 +3,16 @@
 set -ex
 
 # pip config set global.index-url https://mirrors.vmatrix.org.cn/pypi/web/simple
+
+if [ $(uname -m) = 'armv7l' ]; then
+  sed -i 's/scikit-learn//g' /root/packages-stdln.txt
+fi
+
 pip install -U -v -r /root/packages-stdln.txt -f https://ext.maku.ml/wheels.html -f https://torch.maku.ml/whl/stable.html
-pip install -U -v -r /root/packages-deps.txt -f https://ext.maku.ml/wheels.html -f https://torch.maku.ml/whl/stable.html
+
+if [ $(uname -m) = 'aarch64' ]; then
+  pip install -U -v -r /root/packages-deps.txt -f https://ext.maku.ml/wheels.html -f https://torch.maku.ml/whl/stable.html
+fi
+
 mkdir /root/whl
 cp -a $(find /root/.cache/pip | grep whl | tr '\n' ' ') /root/whl/ || echo "No new whl is built!"
